8506019a947833b3acd69e3f85d71e68204de5bf
zyy <2211591840@qq.com>
2019-12-12 18:29:27 +0800

    mvc_first

--- springmvc10/src/main/java/com/example/springmvc10/Controller/ReigsterController.java
+++ springmvc10/src/main/java/com/example/springmvc10/Controller/ReigsterController.java
@@ -3,21 +3,22 @@ package com.example.springmvc10.Controller;
 import com.example.springmvc10.Entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ReigsterController {
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    @ResponseBody
+    @RequestMapping("/login")
     public ModelAndView home(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("register");
         return mv;
     }
-
-    @PostMapping(value = "/register")
+    @PostMapping("/register")
     public ModelAndView login(@ModelAttribute User user){
        /* User user = new User();
         user.setUsername(username);
@@ -27,10 +28,5 @@ public class ReigsterController {
         mv.addObject("user",user);
         mv.setViewName("success");
         return mv;
-    }
-
-    @RequestMapping("/test")
-    public String test(){
-        return "success!!";
     }
 }
